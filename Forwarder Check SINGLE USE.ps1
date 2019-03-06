8a8543a003ef44adb786954561483dba643cf80f
Superfluxus <38507824+Superfluxus@users.noreply.github.com>
2019-03-06 11:13:13 +0000

    Add files via upload

---
+++ Forwarder Check SINGLE USE.ps1
@@ -0,0 +1,42 @@
+﻿$credentials = Get-Credential
+Write-Output "Retrieving CMDlets..."
+    
+$Session = New-PSSession -ConnectionUri https://outlook.office365.com/powershell-liveid/ `
+    -ConfigurationName Microsoft.Exchange -Credential $credentials `
+    -Authentication Basic -AllowRedirection
+Import-PSSession $Session
+ 
+$mailboxes = Get-Mailbox -ResultSize Unlimited
+$domains = Get-AcceptedDomain
+  
+foreach ($mailbox in $mailboxes) {
+  
+    $forwardingSMTPAddress = $null
+    Write-Host "Checking forwarding for $($mailbox.displayname) - $($mailbox.primarysmtpaddress)"
+    $forwardingSMTPAddress = $mailbox.forwardingsmtpaddress
+    $externalRecipient = $null
+    if ($forwardingSMTPAddress) {
+        $email = ($forwardingSMTPAddress -split "SMTP:")[1]
+        $domain = ($email -split "@")[1]
+        if ($domains.DomainName -notcontains $domain) {
+            $externalRecipient = $email
+        }
+  
+        if ($externalRecipient) {
+            Write-Host "$($mailbox.displayname) - $($mailbox.primarysmtpaddress) forwards to $externalRecipient" -ForegroundColor Yellow
+  
+            $forwardHash = $null
+            $forwardHash = [ordered]@{
+                PrimarySmtpAddress = $mailbox.PrimarySmtpAddress
+                DisplayName        = $mailbox.DisplayName
+                ExternalRecipient  = $externalRecipient
+            }
+           $ruleObject = New-Object PSObject -Property $forwardHash
+           $ruleObject|Format-List
+           $ruleObject | Out-File C:\temp\ExternalForward.txt -Encoding utf8 -Append
+        }
+    }
+}
+Remove-PSSession -id 1
+
+### NEED TO WORK ON EXPORTING FILE
